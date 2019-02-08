0b513e4ab8677fd681ae6b7f176828ca9e992f76
CheeCheeBee <47338653+CheeCheeBee@users.noreply.github.com>
2019-02-08 11:01:03 -0500

    Update neopixel_1.ino

--- neopixel_1.ino
+++ neopixel_1.ino
@@ -2,7 +2,10 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <neopixel.h>
 
-#define PIXEL_PIN A5   // IMPORTANT: Set pixel COUNT, PIN and TYPE
+// This #include statement was automatically added by the Particle IDE.
+#include <neopixel.h>
+
+#define PIXEL_PIN A5                                        // IMPORTANT: Set pixel COUNT, PIN and TYPE
 #define PIXEL_COUNT 29
 #define PIXEL_TYPE WS2812
  
@@ -10,110 +13,108 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 int neoPixel = A5;
 int buttonPin = D3;
+int brightness = 0;
+unsigned long now = 0;
+unsigned long lastPublish = 0;
+unsigned long lastFade = 0;
 
+int ledsHalfOff = 1 * 60 * 1000; // 1 minute
+int ledsRed = 30 * 1000; // 30 seconds
+int ledsFlash15s = 15 * 1000; // 15 seconds
+int ledsFlashFiveTimes = 0 * 1000;  // Zero seconds
 
-void setup() {
+uint16_t i;
+uint32_t c = strip.Color( 255, 0, 0 ); 
+uint32_t d = strip.Color( 0, 0, 0 ); 
+
+void setup()
+{
     
-    pinMode( buttonPin, INPUT_PULLUP);
+    pinMode( buttonPin, INPUT_PULLUP); // sets pin as input
     pinMode( neoPixel, OUTPUT );
   
     strip.begin();
-    strip.show();
-    strip.numPixels();
+    strip.show();   // Initialize all pixels to 'off'
+    strip.numPixels();  // find the number of pixels
+    
 }
 
 void loop() {
+    
+    int buttonState = digitalRead( buttonPin );
+    brightness = strip.Color(255, 255, 255); 
+    
+    unsigned long now = millis();
+	if ((now - lastPublish) >= 1000) 
+	{
+		lastPublish = now;
+		Particle.publish( "start", String(now));
+	}
 
-int buttonState = digitalRead( buttonPin );
-    uint16_t i;
-    uint32_t c = strip.Color(0, 0, 255);
-    uint32_t a = strip.Color(0, 0, 0);
-
- if( buttonState == LOW )
- {
-    digitalWrite( neoPixel, HIGH);
-
-    for(i=0; i < strip.numPixels(); ++i)
-    {
-        strip.setPixelColor(i, c );
-		    strip.show();
-		    delay( 100 );
-		    
-    }
-    }else if(buttonState == HIGH)
+    if( buttonState == LOW && ((now - lastPublish) >= 10000))   
     {
     
-    for(i=0; i < strip.numPixels(); ++i) {
-        strip.setPixelColor((29-i), a );
-		    strip.show();
-		    delay( 100 );
+        lastPublish = now;
+		Particle.publish( "brightness", String(now));
+    
+        digitalWrite( neoPixel, HIGH);  // turn the LED On
+        for(i=0; i < strip.numPixels(); ++i) // incrememnt up
+        {
+        strip.setPixelColor(i, c ); // pixel number, brightness
+		strip.show();
+		delay( 100 );
+        }
     }
+    
 
-}
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// VISUALLY, CONCEPTUALLY WHAT I WANT TO HAVE HAPPEN, doesn't work >> WILL PUT IN TIME (MILLIS) SOON
-
-void loop(){
-
-	// declare color variables	
-	// check time (is the timer a count down or timer up?)
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// TURN ON ALL COLOR STRIP FOR 2 MINUTES
-
-if (currentTime < 120000){  // 120 seconds
-	digital Write (neoPixlel, HIGH);
-
-		for(i=0; i < strip.numPixels(); ++i) 
-    	{
-        	strip.setPixelColor(i, c );
-		strip.show();
-		delay( 0 );
-	}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// AFTER 1 MINUTE, TURN HALF OF THE PIXELS TO RED
-
-} else if (currentTime < 60000 && currentTime > 15000){ 	// 60s-15 seconds	
-		for(i=0; i < 29 && > 14; --i) // increment up
+    if ((now + ledsHalfOff) >= 6000)
 	{
-		strip.setPixelColor( i, a ); 
+		lastPublish = now;
+		Particle.publish( "leds half off", String(now));
+	
+    for(i=0; i < strip.numPixels(); ++i) // incrememnt up
+    {
+        strip.setPixelColor((29-14), c );
 		strip.show();
-		delay( 0 );
-	}
-	}
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// AFTER
-
-} else if (currentTime < 15000){ 		// 30-15 seconds
-	digitalWrite( neoPixel, HIGH);  // turn the LED On
+		delay( 100 );
 		
-		for(i=0; i < strip.numPixels(); ++i) // incrememnt up
-	{
-	strip.setPixelColor(i, a ); // pixel number, brightness
-		 strip.show();
-		delay( 0 );
+		strip.setPixelColor((13-0), d ); 
+		strip.show();
+		delay( 100 );
+    }
 	}
+    
+
+    if((now + ledsRed) >= 9000)
+	{
+		lastPublish = now;
+		Particle.publish( "leds red", String(now));
+	
+    for(i=0; i < strip.numPixels(); ++i)
+    {
+        strip.setPixelColor(i, c ); // pixel number, brightness
+		strip.show();
+		delay( 0 );
+    }
+	}
+
+    if ((now + ledsFlash15s) >= 10500)
+	{
+		lastPublish = now;
+		Particle.publish( "flash 15", String(now));
+   
+   for(i=0; i < 5; ++i)
+    {	
+        digitalWrite( neoPixel, HIGH);
+        strip.show();
+	    delay( 50 );
+
+        digitalWrite( neoPixel, LOW);
+        strip.show();
+	    delay( 50 );
+    }
 	}
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-}else if (currentTime <=1500 && current = 0){
-	digitalWrite( neoPixel, HIGH);  // turn the LED On
-		    strip.show();
-		    delay( 5000 );
-	digitalWrite( neoPixel, LOW);  // turn the LED off
-		    strip.show();
-		    delay( 5000 );
+      
+        
+    
